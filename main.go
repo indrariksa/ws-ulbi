06670194e22f036d606b5cc28734b266285c798d
Indra Riksa Herlambang <indrariksa@gmail.com>
2023-03-16 11:40:56 +0700

    push wsulbi nih

--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"log"
 
-	"iteung/config"
+	"github.com/indrariksa/ws-ulbi/config"
 
 	"github.com/aiteung/musik"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 
 	"github.com/whatsauth/whatsauth"
 
-	"iteung/url"
+	"github.com/indrariksa/ws-ulbi/url"
 
 	"github.com/gofiber/fiber/v2"
 )
