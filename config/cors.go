41add9adc6c2aef7549fe866ba44dafd572dc637
Indra Riksa Herlambang <indrariksa@gmail.com>
2023-03-21 22:53:14 +0700

    update cors

--- config/cors.go
+++ config/cors.go
@@ -18,6 +18,7 @@ var origins = []string{
 	"https://whatsauth.github.io",
 	"http://127.0.0.1:5500",
 	"http://127.0.0.1:8080",
+	"https://ws-ulbi.herokuapp.com",
 	"https://indrariksa.github.io/",
 }
 
