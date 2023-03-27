395abf326f90d0936eba10910dbf4a9578c8391e
indrariksa <indrariksa@gmail.com>
2023-03-27 13:29:29 +0700

    tambah cors

--- config/cors.go
+++ config/cors.go
@@ -17,6 +17,7 @@ var origins = []string{
 	"https://iteung.ulbi.ac.id",
 	"https://whatsauth.github.io",
 	"http://127.0.0.1:5500",
+	"http://127.0.0.1:5501",
 	"http://127.0.0.1:8080",
 	"https://ws-ulbi.herokuapp.com",
 	"https://indrariksa.github.io",
