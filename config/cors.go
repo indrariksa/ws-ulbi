0bb32e1ee5a18e2136313d88ba496a004d428e9d
indrariksa <indrariksa@gmail.com>
2023-03-29 12:07:27 +0700

    update cors

--- config/cors.go
+++ config/cors.go
@@ -19,8 +19,7 @@ var origins = []string{
 	"http://127.0.0.1:5500",
 	"http://127.0.0.1:5501",
 	"http://127.0.0.1:8080",
-	"https://ws-ulbi.herokuapp.com",
-	//"https://indrariksa.github.io",
+	"https://indrariksa.github.io",
 }
 
 var Internalhost string = os.Getenv("INTERNALHOST") + ":" + os.Getenv("PORT")
